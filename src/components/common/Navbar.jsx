6d22906559a24ee25f4a873f88dcce6ca6f782f3
AnukaFonseka <akunafonseka@gmail.com>
2024-12-02 15:35:57 +0530

    navbar update

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -151,7 +151,7 @@ function Navbar() {
           </span>
         </a>
         <a
-          href="https://drive.google.com/file/d/1Xc_jCwd075Uz2obHd84ZNtyIrXJCVNPs/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1b1ezHqgau4B2e4uRZHfnwAh5NOLeG2Gg/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center text-black hover:text-blue-500 transition"
